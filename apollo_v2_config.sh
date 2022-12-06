fbc4c65be8bb8cbf96f0a926eaf7a0dbd0a7c3fa
Zachary J. Fields <zachary_fields@yahoo.com>
2022-12-06 13:42:48 -0600

    feat: Notecard ONLY Script

---
+++ apollo_v2_config.sh
@@ -0,0 +1,3 @@
+{"req":"card.restore","delete":true}
+{"req":"hub.set","product":"com.zakoverflow.test","sn":"Apollo II","vinbound":"usb:10;high:10080;normal:10080;low:20160;dead:0","voutbound":"usb:10;high:10080;normal:10080;low:20160;dead:0"}
+{"req":"card.motion.sync","start":true,"sync":true}
